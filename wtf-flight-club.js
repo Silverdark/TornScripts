a9be75d1c06cf2313b86ab43737cfa8ce64e1a48
Daniel Hottmeyer <Silverdark@users.noreply.github.com>
2025-04-17 20:26:06 +0200

    Remove fetch from flight crew page

--- wtf-flight-club.js
+++ wtf-flight-club.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WTF Flight Club
 // @namespace    http://tampermonkey.net/
-// @version      2025-04-16
+// @version      2025-04-17.1
 // @description  Flight Club Helper tools
 // @author       Silverdark [3503183]
 // @match        https://www.torn.com/item.php
@@ -11,8 +11,6 @@
 
 (function() {
     'use strict';
-
-    const remainingByItemName = new Map();
 
     // Source: https://stackoverflow.com/a/61511955
     function waitForElm(parent, selector) {
@@ -35,99 +33,6 @@
         });
     }
 
-    function updateItemListEntry(listItem) {
-        const sortId = listItem.dataset.sort;
-        if (!sortId) return;
-
-        let remaining = null;
-        for (let [itemName, val] of remainingByItemName) {
-            if (!sortId.includes(itemName)) continue;
-
-            remaining = val;
-            break;
-        }
-
-        if (!remaining) return;
-
-        const nameWrap = listItem.querySelector(".name-wrap");
-        let remainingFlightClubElement = nameWrap.querySelector(".flight-club-remaining");
-        if (!remainingFlightClubElement) {
-            remainingFlightClubElement = document.createElement("span");
-            remainingFlightClubElement.className = "flight-club-remaining";
-
-            nameWrap.appendChild(remainingFlightClubElement);
-        }
-
-        const remainingText = remaining < 0 ? "Done" : `x${remaining}`;
-        remainingFlightClubElement.innerHTML = `FC: <span class="bold">${remainingText}</span>`;
-    }
-
-    function fetchData(type) {
-        const url = type === "Plushies"
-            ? "https://travel.torn.wtf/dashboard"
-            : "https://travel.torn.wtf/dashboard/flowers";
-
-        return new Promise(resolve => {
-            GM_xmlhttpRequest({
-                method: 'GET',
-                url: url,
-                anonymous: false,
-                onload: function (response) {
-                    if (response.status !== 200) {
-                        console.error(`Request failed with status ${response.status}`);
-                        return;
-                    }
-
-                    if (response.responseText.includes("Please login below with your existing Torn")) {
-                        console.log("No Flight Club cookie available");
-                        return;
-                    }
-
-                    const parser = new DOMParser();
-                    const targetDocument = parser.parseFromString(response.responseText, "text/html");
-
-                    const overviewTable = targetDocument.querySelector("#overview_table");
-                    if (!overviewTable) {
-                        console.error('Overview table not found in response');
-                        return;
-                    }
-
-                    for (let row of Array.from(overviewTable.rows).slice(1))
-                    {
-                        const cells = row.cells;
-
-                        const itemName = Array.from(cells[0].childNodes)
-                        .find(x => x.nodeType === Node.TEXT_NODE)
-                        .textContent
-                        .trim();
-
-                        const remaining = cells[2].textContent;
-                        remainingByItemName.set(itemName, remaining);
-                    }
-
-                    resolve();
-                },
-                onerror: function(error) {
-                    console.error('Request failed:', error);
-                }
-            });
-        });
-    }
-
-    // Changes in category
-    waitForElm(document.body, "#category-wrap").then(categoryWrapper => {
-        const observer = new MutationObserver((mutations) => {
-            for (const mutation of mutations) {
-                const mutationTarget = mutation.target;
-                updateItemListEntry(mutationTarget);
-            }
-        });
-
-        observer.observe(categoryWrapper, {
-            subtree: true,
-            attributes: true
-        });
-    });
 
     // Flight Club Button handling
     document.addEventListener("click", function (evt) {
@@ -165,13 +70,4 @@
             btnWrapNode.prepend(flightClubBtn);
         });
     }, false);
-
-    // Fetch data from Flight Club page
-    Promise.all(fetchData("Plushies"), fetchData("Flowers")).then(() => {
-        const plushies = Array.from(document.querySelector("#plushies-items").getElementsByTagName("li"));
-        plushies.forEach(x => updateItemListEntry(x));
-
-        const flowers = Array.from(document.querySelector("#flowers-items").getElementsByTagName("li"));
-        flowers.forEach(x => updateItemListEntry(x));
-    });
 })();
